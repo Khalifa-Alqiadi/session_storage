82ffa14eea623f21f9f3586d81ba24140a93d9af
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-03-08 21:04:21 +0300

    commit initr

--- include/templates/header.php
+++ include/templates/header.php
@@ -4,8 +4,7 @@
          <meta charset="UTF-8" />
          <title></title>
          <link rel="stylesheet" href="<?php echo $css ?>bootstrap.min.css">
-         <link rel="stylesheet" href="<?php echo $css ?>font-awesome.min.css">
-         <link rel="stylesheet" href="<?php echo $css ?>jquery-ui.css">
+         <!-- <link rel="stylesheet" href="<?php //echo $css ?>jquery-ui.css"> -->
          <link rel="stylesheet" href="<?php echo $css ?>control.css">
     </head>
 <body>
